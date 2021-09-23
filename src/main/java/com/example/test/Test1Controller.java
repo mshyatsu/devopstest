b3fd6031cb4b125057d986f915c3d54a69eb3bb7
mshyatsu <mshyatsu>
2021-09-23 16:45:29 +0900

    test

--- src/main/java/com/example/test/Test1Controller.java
+++ src/main/java/com/example/test/Test1Controller.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class Test1Controller {
-	@RequestMapping(value="/testproject1")
+	@RequestMapping(value="/")
 	private String index(){
 	return "/index.html";
 	}
